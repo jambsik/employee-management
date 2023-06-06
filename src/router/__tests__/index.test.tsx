19af8e3c1ed98044966a40bf9b05cccd6cfe90b2
John Muñoz <jambsik@gmail.com>
2023-06-06 18:28:16 +0200

    Fix pipeline

--- src/router/__tests__/index.test.tsx
+++ src/router/__tests__/index.test.tsx
@@ -1,19 +1,10 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import { Employees } from '../../pages/Employees';
-import { ErrorPage } from '../../pages/Error';
-import { Route } from '../../constants/Route';
+import { getRoutes } from '..';
 
 describe('getRoutes tests', () => {
     it('Should match all expected routes', () => {
-        const routes = createBrowserRouter([
-            {
-                path: Route.Employee,
-                element: <Employees />,
-                errorElement: <ErrorPage />,
-            },
-        ]);
-        const { getByText } = render(<RouterProvider router={routes} />);
-        expect(getByText('Employee component')).toBeInTheDocument();
+        const { getByText } = render(<RouterProvider router={getRoutes()} />);
+        expect(getByText('navigation.title')).toBeInTheDocument();
     });
 });
